0676886a4aeebdc5277b3790b9bb05f8055f35c3
Mi1toFam <miltogeleta@gmail.com>
2021-07-06 19:04:46 -0400

    Logging Out + User Persistence

--- Instagram/View Controllers/FeedViewController.m
+++ Instagram/View Controllers/FeedViewController.m
@@ -6,6 +6,9 @@
 //
 
 #import "FeedViewController.h"
+#import <Parse/Parse.h>
+#import "SceneDelegate.h"
+#import "LoginViewController.h"
 
 @interface FeedViewController ()
 
@@ -16,6 +19,19 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view.
+}
+
+// function for logging user out of app, bringing them back to the login page
+- (IBAction)didTapLogout:(id)sender {
+    SceneDelegate *myDelegate = (SceneDelegate *)self.view.window.windowScene.delegate;
+
+    UIStoryboard *storyboard = [UIStoryboard storyboardWithName:@"Main" bundle:nil];
+    LoginViewController *loginViewController = [storyboard instantiateViewControllerWithIdentifier:@"LoginViewController"];
+    myDelegate.window.rootViewController = loginViewController;
+    
+    [PFUser logOutInBackgroundWithBlock:^(NSError * _Nullable error) {
+        // PFUser.current() will now be nil
+    }];
 }
 
 /*
